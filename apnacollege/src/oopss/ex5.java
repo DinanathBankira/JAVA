4cbae06dec9bc0fb54a5a57ba9e679e5b5c7cc48
Dina <dinanath88090@outlook.com>
2024-04-25 10:01:52 +0530

    Encapsulation

--- apnacollege/src/oopss/ex5.java
+++ apnacollege/src/oopss/ex5.java
@@ -1,13 +1,23 @@
 package oopss;
-
+	//polimorphism
+	//Inheritance
+	//Encapsulation
+	//Objacts
+	//Class
+	//Abstraction
+class book{
+	private String Book;
+	public String getName() {
+		return Book;
+	}
+	public void setName(String name) {
+		Book=name;
+	}
+}
 public class ex5 {
 	public static void main(String[] args) {
-//		polimorphism
-//		Inheritance
-//		Encapsulation
-//		Objacts
-//		Class
-//		Abstraction
-		System.out.println("Hello Github");
+		book b=new book();
+		b.setName("The Story of Thaj Mahal");
+		System.out.println(b.getName());
 	}
 }
